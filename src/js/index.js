2631c8498b41582dc7d39c20a649b8dc2700ca16
James Randolph <james.randolph2016@gmail.com>
2019-07-31 17:10:17 -0400

    made account on food2fork and have succesfully logged api requests

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
-// Global app controller
-import num from './test';
+import axios from 'axios';
 
-console.log(`I imported ${num} from another module ssss`);
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '771f9a5b4400d39be13af98219a9aa5a';
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`)
+        const recipes = res.data.recipes
+        console.log(recipes);
+
+    } catch (error) {
+        alert(error)
+    }
+};
+getResults('ramen');
 
