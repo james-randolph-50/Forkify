6363c361e5f0c0216cb86823e65ac350d8e067c1
James Randolph <james.randolph2016@gmail.com>
2019-08-01 15:40:20 -0400

    takes search value and runs reuqest for it on search

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,26 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 // GLOBAL STATE OF APP
 const state = {};
 
-const controlSearch = () => {
+const controlSearch = async () => {
+    const query = searchView.getInput();
+    console.log(query)
+
+    if (query) {
+        state.search = new Search(query);
+
+       await state.search.getResults();
+
+       console.log(state.search.result);
+    }
 
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 })
 
-const search = new Search('ramen');
-// console.log(search);
-// search.getResults();///
