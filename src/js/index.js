3f6a93c1c89f59ea2fc244b723bb23f13d19e998
James Randolph <james.randolph2016@gmail.com>
2019-08-01 15:14:21 -0400

    getResults returns resuly

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '771f9a5b4400d39be13af98219a9aa5a';
-    try {
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`)
-        const recipes = res.data.recipes
-        console.log(recipes);
-
-    } catch (error) {
-        alert(error)
-    }
-};
-getResults('ramen');
-
+const search = new Search('ramen');
+// console.log(search);
+// search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+    
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '771f9a5b4400d39be13af98219a9aa5a';
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
+            this.result = res.data.recipes
+            // console.log(this.result);
+        } catch (error) {
+            alert(error)
+        }
+    };
+}
+
+
