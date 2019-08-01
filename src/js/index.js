c0623be925b013728343ad15ebd6edce479d926f
James Randolph <james.randolph2016@gmail.com>
2019-08-01 15:57:03 -0400

    add html markup with dynamic values

--- src/js/index.js
+++ src/js/index.js
@@ -7,16 +7,14 @@ const state = {};
 
 const controlSearch = async () => {
     const query = searchView.getInput();
-    console.log(query)
 
     if (query) {
         state.search = new Search(query);
 
        await state.search.getResults();
 
-       console.log(state.search.result);
+        searchView.renderResults(state.search.result)
     }
-
 }
 
 elements.searchForm.addEventListener('submit', e => {
