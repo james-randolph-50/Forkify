c08b77ad7acf5364981e9b5d756189ca8eaee1e4
James Randolph <james.randolph2016@gmail.com>
2019-08-01 15:19:47 -0400

    add controlSeaarch fucntion

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,17 @@
 import Search from './models/Search';
 
+// GLOBAL STATE OF APP
+const state = {};
+
+const controlSearch = () => {
+
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
+
 const search = new Search('ramen');
 // console.log(search);
-// search.getResults();
+// search.getResults();///
