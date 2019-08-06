50347393bb50bcdb90f708275110503df4caa3e2
James Randolph <james.randolph2016@gmail.com>
2019-08-06 12:30:19 -0400

    clear prev results to load ten more results

--- src/js/index.js
+++ src/js/index.js
@@ -30,10 +30,9 @@ elements.searchForm.addEventListener('submit', e => {
 elements.searchResPages.addEventListener('click', e => {
     const btn = e.target.closest('.btn-inline');
     if (btn) {
-        console.log(btn);
         const goToPage = parseInt(btn.dataset.goto, 10);
-        console.log(goToPage)
+        searchView.clearResults();
         searchView.renderResults(state.search.result, goToPage);
-    }
-})
+    };
+});
 
